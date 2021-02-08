42d5e8dce2b5ed862253b2c5cba97f8c1de120e3
0giru <dusrlf13@gmail.com>
2021-02-09 01:58:37 +0900

    boj_2606

--- boj_2606.py
+++ boj_2606.py
@@ -5,18 +5,31 @@ N = int(input())
 M = int(input())
 
 queue = deque()
+result = []
 
-visited = [False] * [N+1]
+visited = [False] * (N+1)
 
-# 컴퓨터의 수 만큼 빈 리스트를 만들어 그래프를 만들거임
 graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
     i, j = map(int, input().split())
     graph[i].append(j)
+    graph[j].append(i)
 
 for i in range(len(graph)):
     graph[i].sort()
 
 def bfs(par_graph,par_start, par_visited):
+    queue.append(par_start)
+    visited[par_start] = True
+    while queue:
+        temp = queue.popleft()
+        result.append(temp)
+        for i in par_graph[temp]:
+            if not par_visited[i]:
+                queue.append(i)
+                par_visited[i] = True
 
+bfs(graph, 1, visited)
+
+print(len(result)-1)
