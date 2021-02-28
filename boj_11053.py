6757979f5ca8fd13d6034f95b4ce3be06be9b4ce
0giru <dusrlf13@gmail.com>
2021-02-28 19:03:33 +0900

    2021-02-28

--- boj_11053.py
+++ boj_11053.py
@@ -1,14 +1,29 @@
+# 가장 긴 증가하는 부분수열 https://www.acmicpc.net/problem/11053
+
 N = int(input())
 
 array = list(map(int, input().split()))
 dp = [1 for i in range(N)]
+temp_list = list()
 
+# 다시보니 코드가 이해가 안돼 다시품
 for i in range(N):
     for j in range(i):
-        if array[i] > array[j] and dp[j] + 1 > dp[i]:
-            dp[i] = dp[j] + 1
+        if array[i] > array[j]:
+            temp_list.append(dp[j]+1)
+            temp = max(temp_list)
+            dp[i] = temp
+    temp_list.clear()
 
 print(max(dp))
+
+# 이 코드도 맞는 코드
+# for i in range(N):
+#     for j in range(i):
+#         if array[i] > array[j] and dp[j] + 1 > dp[i]:
+#             dp[i] = dp[j] + 1
+
+# print(max(dp))
 
 # 완전탐색 풀이 - 시간초과
 # for i in range(N):
