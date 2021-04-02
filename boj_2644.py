bb940adc38fdd5ea466786ddeb1e6339dba4c9d4
0giru <dusrlf2003@naver.com>
2021-04-02 22:18:56 +0900

    20210402

--- boj_2644.py
+++ boj_2644.py
@@ -1,87 +1,41 @@
 # 촌수계산 https://www.acmicpc.net/problem/2644
+from collections import deque
+
+queue = deque()
 
 n = int(input())
-
+# 찾을 사람 2명 입력
 p1, p2 = map(int, input().split())
-
 m = int(input())
 
-graph = [[] for _ in range(101)]
-count = 0
+visited = [False] * (n+1)
+
+array = [[] for _ in range(n+1)]
+count = [0] * (n+1)
 
 for _ in range(m):
-    # x는 y의 부모
-    x, y = map(int, input().split())
-    graph[x].append(y)
+    # 부모 자식 관계 입력
+    p, d = map(int, input().split())
+    array[p].append(d)
+    array[d].append(p)
 
-# p1의 부모 알아내기
-def findP1(p1):
-    global count
-    index = 0
-    for i in range(1, 101):
-        if p1 in graph[i]:
-            count+=1
-            index = i
-            return index
+queue.append(p1)
+
+while queue:
+    temp = queue.popleft()
+    visited[temp] = True
+
+    for i in array[temp]:
+        if not visited[i]:
+            queue.append(i)
+
+    for i in array[temp]:
+        in not visited[i]:
+            count[i] = count[temp]+1
         else:
             continue
-    # 부모가 없는 최종 노드라면 자기자신을 반환
-    index = p1
-    return index
 
-# p2의 부모 알아내기
-def findP2(p2):
-    global count
-    index = 0
-    for i in range(1, 101):
-        if p2 in graph[i]:
-            count+=1
-            index = i
-            return index
-        else:
-            continue
-    # 부모가 없는 최종 노드라면 자기자신을 반환
-    index = p2
-    return index
-
-def Search(p1, p2):
-    global count
-    last1 = False
-    last2 = False
-
-    P1 = findP1(p1)
-    P2 = findP2(p2)
-
-    # 두 노드의 부모가 동일하다면 출력 후 종료
-    if P1 == P2:
-        print(count)
-        return
-    
-    for i in range(1, 101):
-        if P1 in graph[P2]:
-            print(count+1)
-            return
-        elif P2 in graph[P1]:
-            print(count+1)
-            return
-
-    # 최종 노드인지 검사
-    for i in range(1, 101):
-        if P1 not in graph[i]:
-            last1 = True
-        else:
-            break
-    for i in range(1, 101):
-        if P2 not in graph[i]:
-            last2 = True
-        else:
-            break
-
-    # 연관 없는 최종 노드뿐이라면 촌수 관계 없음
-    if last1 == True and last2 == True:
-        print(-1)
-        return
-
-    Search(P1, P2)
-
-Search(p1, p2)
+if count[p2] != 0:
+    print(count[p2])
+else:
+    print(-1)
